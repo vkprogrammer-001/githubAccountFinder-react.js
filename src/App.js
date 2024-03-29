30df751ab1359ce0855c2ebf4469c0a990e87bcf
vkprogrammer-001 <vipinkumar212003@gmail.com>
2024-03-30 00:33:02 +0530

    update file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from "react";
+import "./App.css";
+import UserCard from "./components/user-card/user-card";
 
-function App() {
+
+export default function App() {
+  
+  // useState : is used to set state variables .
+  // we will use this useStates to save user input and api result data .
+  
+  const [inputValue, setInputValue] = useState("");
+
+  const [results, setResults] = useState([]);
+
+
+  function handleOnChange(event) {
+    // console.log(event)
+    setInputValue(event.target.value);
+  }
+
+
+
+  //  js  : synchronous : step by step 
+
+
+  // line 1
+  // line 2
+  //  line 3
+
+  async function onSearchSubmit() {
+    //  line one : geting data from api 
+    //  rsetting the response data
+
+    const results =  await findGithubAccounts(inputValue);
+
+    // console.log(results[0])
+    setResults(results);
+
+  }
+
+  const API_URL = "https://api.github.com";
+
+  async function findGithubAccounts(query) {
+    try {
+      const response = await fetch(`${API_URL}/search/users?q=${query}`);
+      const json = await response.json();
+      return json.items || [];
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  // function foo (query) {
+  //   const result = []
+  //   fetch(`${API_URL}/search/users?q=${query}`)
+  //   .then((response) => response.json())
+  //   .then(data => {
+  //     setResults(data?.items)
+  //   })
+  //   .catch(error => console.log(error))
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main className="main">
+
+        <h2>Project 5: GitHub Account Finder</h2>
+
+        <div className="search-form">
+
+            <input
+              placeholder="Enter username or email"
+              // onChange={handleOnChange}
+              onChange={handleOnChange}
+            />
+            <button 
+              onClick={onSearchSubmit}
+              >Search</button>
+
+        </div>
+
+          <h3>Results</h3>
+          <div id="results">
+            <Fragment>
+              {results.map((user) => (
+                <UserCard
+                  profileLink={user.avatar_url}
+                  accountLink={user.html_url}
+                  username={user.login}
+                />
+              ))}
+            </Fragment>
+          </div>
+      </main>
   );
 }
-
-export default App;
